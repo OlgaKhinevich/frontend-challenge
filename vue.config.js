360fce1640a31f75e1aafebbc58f675f559782db
Olga <olga-kkhinvich@yandex.ru>
2022-05-30 00:10:56 +0300

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/frontend-challenge/" : "/",
   transpileDependencies: true,
   css: {
     loaderOptions: {
